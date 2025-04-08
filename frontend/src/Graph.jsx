47cb9ae1ee87cab2799026464428d288fd02bedf
Byron Tung <btung1@ualberta.ca>
2025-04-08 15:04:00 -0600

    adding general scaffolding and examples that work with backend, need to use css to resize container for graphs

--- frontend/src/Graph.jsx
+++ frontend/src/Graph.jsx
@@ -1,58 +1,136 @@
 import { Chart as ChartJS } from "chart.js/auto";
 import { Line, Bar, Doughnut } from "react-chartjs-2";
-import revenueData from "./assets/revenueData.json"
-import './Graph.css'
+import revenueData from "./assets/revenueData.json";
+import "./Graph.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Graph() {
-  return (
-    <>
-      <Line 
-        data={{
-          labels: revenueData.map((data) => data.label),
-          datasets: [{
-            label: "Revenue",
-            data: revenueData.map((data) => data.revenue),
-            backgroundColor: "#85BB65",
-            borderColor: "#85BB65"
-          },
-          {
-            label: "Cost",
-            data: revenueData.map((data) => data.cost),
-            backgroundColor: "#E5446D",
-            borderColor: "#E5446D"
-          }]
-        }}/>
-      <Bar 
-        data={{
-          labels: revenueData.map((data) => data.label),
-          datasets: [{
-            label: "Revenue",
-            data: revenueData.map((data) => data.revenue),
-            backgroundColor: "#85BB65",
-            borderColor: "#85BB65"
-          },
-          {
-            label: "Cost",
-            data: revenueData.map((data) => data.cost),
-            backgroundColor: "#E5446D",
-            borderColor: "#E5446D"
-          }]
-        }}/>
-      <Doughnut 
-        data={{
-          labels: revenueData.map((data) => data.label),
-          datasets: [{
-            label: "Revenue",
-            data: revenueData.map((data) => data.revenue),
-          },
-          {
-            label: "Cost",
-            data: revenueData.map((data) => data.cost),
-          }]
-        }}/>
-    </>
+    const [carData, setCarData] = useState(null);
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3000/api/makes")
+            .then((res) => setCarData(res.data))
+            .catch((err) => console.error(err));
+    }, []);
+
+    if (!carData){
+        return
+    }
+    console.log(JSON.stringify(carData));
+    return (
+        <>
+            <Bar
+                data={{
+                    labels: carData.map((data) => data.make),
+                    datasets: [
+                        {
+                            label: "count",
+                            data: carData.map((data) => data.count),
+                            backgroundColor: "#85BB65",
+                            borderColor: "#85BB65",
+                        },
+                    ],
+                }}
+            />
+            <Bar
+                data={{
+                    labels: carData.map((data) => data.make),
+                    datasets: [
+                        {
+                            label: "count",
+                            data: carData.map((data) => Math.log(data.count)),
+                            backgroundColor: "#85BB65",
+                            borderColor: "#85BB65",
+                        },
+                    ],
+                }}
+            />
+            <Doughnut
+                data={{
+                    labels: carData.map((data) => data.make),
+                    datasets: [
+                        {
+                            label: "count",
+                            data: carData.map((data) => data.count)
+                        },
+                    ],
+                }}
+            />
+            {/* <Bar
+                data={{
+                    labels: revenueData.map((data) => data.label),
+                    datasets: [
+                        {
+                            label: "Revenue",
+                            data: revenueData.map((data) => data.revenue),
+                            backgroundColor: "#85BB65",
+                            borderColor: "#85BB65",
+                        },
+                        {
+                            label: "Cost",
+                            data: revenueData.map((data) => data.cost),
+                            backgroundColor: "#E5446D",
+                            borderColor: "#E5446D",
+                        },
+                    ],
+                }}
+            /> */}
+        </>
     );
 }
+
+// function Graph() {
+//   return (
+//     <>
+//       <Line
+//         data={{
+//           labels: revenueData.map((data) => data.label),
+//           datasets: [{
+//             label: "Revenue",
+//             data: revenueData.map((data) => data.revenue),
+//             backgroundColor: "#85BB65",
+//             borderColor: "#85BB65"
+//           },
+//           {
+//             label: "Cost",
+//             data: revenueData.map((data) => data.cost),
+//             backgroundColor: "#E5446D",
+//             borderColor: "#E5446D"
+//           }]
+//         }}/>
+//       <Bar
+//         data={{
+//           labels: revenueData.map((data) => data.label),
+//           datasets: [{
+//             label: "Revenue",
+//             data: revenueData.map((data) => data.revenue),
+//             backgroundColor: "#85BB65",
+//             borderColor: "#85BB65"
+//           },
+//           {
+//             label: "Cost",
+//             data: revenueData.map((data) => data.cost),
+//             backgroundColor: "#E5446D",
+//             borderColor: "#E5446D"
+//           }]
+//         }}/>
+//       <Doughnut
+//         data={{
+//           labels: revenueData.map((data) => data.label),
+//           datasets: [{
+//             label: "Revenue",
+//             data: revenueData.map((data) => data.revenue),
+//           },
+//           {
+//             label: "Cost",
+//             data: revenueData.map((data) => data.cost),
+//           }]
+//         }}/>
+//     </>
+//     );
+// }
 
 // function Graph() {
 //   return (
